30f2935a3694cd387f9b9e3229a5bb219ee1b29a
qodot <waitingforqodot@gmail.com>
2019-07-14 02:03:36 +0900

    티어 세분화

--- src/tier/models.py
+++ src/tier/models.py
@@ -1,5 +1,5 @@
 class Tier():
-    MAX_NO_COMMIT_DAY = 999
+    no_commit_day = 999
 
     @classmethod
     def name(cls) -> str:
@@ -9,51 +9,146 @@ class Tier():
     def satisfy(cls, user_average: int, user_no_commit_day: int) -> bool:
         return (
             cls.average <= user_average
-            and cls.no_commit_day > user_no_commit_day
+            and cls.no_commit_day >= user_no_commit_day
         )
 
 
-class Bronze(Tier):
+class Bronze5(Tier):
     average = 0
-    no_commit_day = Tier.MAX_NO_COMMIT_DAY
 
 
-class Silver(Tier):
+class Bronze4(Tier):
+    average = 0.1
+
+
+class Bronze3(Tier):
+    average = 0.2
+
+
+class Bronze2(Tier):
+    average = 0.3
+
+
+class Bronze1(Tier):
+    average = 0.4
+
+
+class Silver5(Tier):
     average = 0.5
-    no_commit_day = Tier.MAX_NO_COMMIT_DAY
 
 
-class Gold(Tier):
+class Silver4(Tier):
+    average = 0.6
+
+
+class Silver3(Tier):
+    average = 0.7
+
+
+class Silver2(Tier):
+    average = 0.8
+
+
+class Silver1(Tier):
+    average = 0.9
+
+
+class Gold5(Tier):
     average = 1
-    no_commit_day = Tier.MAX_NO_COMMIT_DAY
 
 
-class Platinum(Tier):
-    average = 3
-    no_commit_day = 168
+class Gold4(Tier):
+    average = 1.2
 
 
-class Diamond(Tier):
+class Gold3(Tier):
+    average = 1.4
+
+
+class Gold2(Tier):
+    average = 1.6
+
+
+class Gold1(Tier):
+    average = 1.8
+
+
+class Platinum5(Tier):
+    average = 2
+    no_commit_day = 150
+
+
+class Platinum4(Tier):
+    average = 2.4
+    no_commit_day = 145
+
+
+class Platinum3(Tier):
+    average = 2.8
+    no_commit_day = 140
+
+
+class Platinum2(Tier):
+    average = 3.2
+    no_commit_day = 135
+
+
+class Platinum1(Tier):
+    average = 3.6
+    no_commit_day = 130
+
+
+class Diamond5(Tier):
     average = 4
+    no_commit_day = 120
+
+
+class Diamond4(Tier):
+    average = 4.5
+    no_commit_day = 110
+
+
+class Diamond3(Tier):
+    average = 5
     no_commit_day = 100
 
 
-class Master(Tier):
-    average = 5
+class Diamond2(Tier):
+    average = 5.5
+    no_commit_day = 90
+
+
+class Diamond1(Tier):
+    average = 6
+    no_commit_day = 80
+
+
+class Master3(Tier):
+    average = 7
+    no_commit_day = 70
+
+
+class Master2(Tier):
+    average = 8
     no_commit_day = 50
 
 
+class Master1(Tier):
+    average = 9
+    no_commit_day = 30
+
+
 class Challenger(Tier):
-    average = 7
+    average = 10
     no_commit_day = 0
 
 
 TIERS = (
-    Bronze,
-    Silver,
-    Gold,
-    Platinum,
-    Diamond,
-    Master,
+    Bronze5, Bronze4, Bronze3, Bronze2, Bronze1,
+    Silver5, Silver4, Silver3, Silver2, Silver1,
+    Gold5, Gold4, Gold3, Gold2, Gold1,
+    Platinum5, Platinum4, Platinum3, Platinum2, Platinum1,
+    Diamond5, Diamond4, Diamond3, Diamond2, Diamond1,
+    Master3, Master2, Master1,
     Challenger,
 )
--- src/tier/services.py
+++ src/tier/services.py
@@ -1,11 +1,11 @@
 from .models import TIERS
-from .models import Bronze
+from .models import Bronze5
 from src.fetcher.models import UserCount
 
 
 class TierCalculateService():
     def calculate(self, user_count: UserCount) -> str:
-        last_tier = Bronze
+        last_tier = Bronze5
         for tier in TIERS:
             if not tier.satisfy(user_count.average, user_count.no_commit_day):
                 break
