2f44fb988bdc4fcbb2764f00eebd0f8d8c56418a
yuyuny <yuyunykk@gmail.com>
2019-07-14 15:42:09 +0900

    WIP animation

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,3 +1,5 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import 'react-hot-loader';
 import 'normalize.css';
 import React from 'react';
--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import styled, { ThemeProvider } from 'styled-components';
+import * as THREE from 'three';
 
 import GlobalStyle from 'styles/global';
 import theme from 'styles/theme';
+
+function testAnimate() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const loader = new THREE.FontLoader();
+
+  loader.load('fonts/helvetiker_bold.typeface.json', font => {
+    console.log(font);
+
+    const geometry = new THREE.TextGeometry('챌린저', {
+      bevelEnabled: true,
+      bevelOffset: 0,
+      bevelSegments: 5,
+      bevelSize: 8,
+      bevelThickness: 10,
+      curveSegments: 12,
+      font,
+      height: 5,
+      size: 80,
+    });
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    camera.position.z = 5;
+  });
+
+  // const animate = function() {
+  //   requestAnimationFrame(animate);
+
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+
+  //   renderer.render(scene, camera);
+  // };
+
+  // animate();
+}
 
 const App: React.FC = () => {
   return (
@@ -12,6 +61,7 @@ const App: React.FC = () => {
         <GlobalStyle />
         <Wrapper>
           <h1>hello world!</h1>
+          {testAnimate()}
         </Wrapper>
       </>
     </ThemeProvider>
